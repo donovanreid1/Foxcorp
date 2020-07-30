f763e8a1baed1cc2eb3eac37a1b3943a08b9d511
donovanreid1 <68993112+donovanreid1@users.noreply.github.com>
2020-07-30 16:16:25 -0700

    Delete managedidentity.ps1

--- managedidentity.ps1
+++
@@ -1,7 +0,0 @@
-# Creates a system assigned managed identity for VM
-$vm = Get-AzVM -ResourceGroupName "devtest" -Name "linuxvm"
-Update-AzVM -ResourceGroupName "devtest" -VM $vm -IdentityType "SystemAssigned"
-
-#Creates a contributor role for that system assigned managed identity scoped to specfic storage account
-$spID = (Get-AzVM -ResourceGroupName "devtest" -Name "linuxvm").identity.principalid
-New-AzRoleAssignment -ObjectId $spID -RoleDefinitionName "Contributor" -Scope "/subscriptions/<917e32fe-361a-4bb7-aada-92f002a05a39>/resourceGroups/<devtest>/providers/Microsoft.Storage/storageAccounts/<istsolutionstesting>"
