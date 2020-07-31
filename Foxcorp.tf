4e2ae70ff9e5abca6c65f32635a2458a8e6b14dc
donovanreid1 <68993112+donovanreid1@users.noreply.github.com>
2020-07-31 01:20:03 -0700

    Update Foxcorp.tf

--- Foxcorp.tf
+++ Foxcorp.tf
@@ -108,7 +108,7 @@ resource "azurerm_role_definition" "foxcorp" {
 
 #scopes role assignment to linuxvm
 resource "azurerm_role_assignment" "foxcorp" {
-  scope              = "/subscriptions/917e32fe-361a-4bb7-aada-92f002a05a39/resourceGroups/devtest/providers/Microsoft.compute/virtualmachines/linuxvm"
+  scope              = "/subscriptions/917e32fe-361a-4bb7-aada-92f002a05a39/resourceGroups/rg1/providers/Microsoft.compute/virtualmachines/linuxvm"
   role_definition_id = azurerm_role_definition.foxcorp.id
   principal_id       = data.azurerm_client_config.foxcorp.client_id
 }
